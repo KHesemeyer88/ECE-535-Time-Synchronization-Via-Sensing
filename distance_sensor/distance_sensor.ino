771c19ae9d0729f3a54626390d946aabc96c7116
KHesemeyer88 <kianhesemeyer@gmail.com>
2024-11-25 19:30:10 -0500

    Co-authored-by: khgreenwood <khgreenwood@users.noreply.github.com>

--- distance_sensor/distance_sensor.ino
+++ distance_sensor/distance_sensor.ino
@@ -8,7 +8,39 @@ void setup() {
   pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
   pinMode(echoPin, INPUT); // Sets the echoPin as an Input
   Serial.begin(9600); // Starts the serial communication
+
+  // Delay handshake
+
+  Serial.println("Begin");
+
+  byte var = 1;
+  byte var2 = 1;
+  int i = 0;
+
+//  byte handshake = 00000001;
+  while (Serial.available() == -1 || Serial.readStringUntil('\n') != "hello") {
+    //wait while serial unavailable 
+    // if either is true stay in the loop 
+    Serial.println("waiting");
+
+    if (i == 10) {
+      Serial.println("hello");
+    }
+    i++;
+  }
+
+  Serial.print("Timestamp ");
+  Serial.println(micros());
+
+
+  //Serial.println(Serial.read(), DEC);
+  while (1) {
+
+  }
 }
+
+
+
 void loop() {
   // Clears the trigPin
   digitalWrite(trigPin, LOW);
@@ -24,5 +56,5 @@ void loop() {
   // Prints the distance on the Serial Monitor
   Serial.print(distance);
   Serial.print(" ");
-  Serial.println(millis());
+  Serial.println(micros());
 }
--- project8.py
+++ project8.py
@@ -11,8 +11,10 @@ serial2 = serial.Serial('/dev/ttyUSB1', 9600)
 
 i = 0
 threshold = 10
-timeline1 = []
-timeline2 = []
+raw_timeline1 = []
+raw_timeline2 = []
+sync_timeline1 = []
+sync_timeline2 = []
 drift1 = []
 drift2 = []
 relativeDrift = []
@@ -23,6 +25,32 @@ eventIndex = 0
 offset1 = 0
 offset2 = 0
 
+
+# delay calcualation
+    #1. esp32 things waits
+    #2. raspberry pi timestamps(1), then sends handshake
+    #3. esp32 recives handshake, timestamps(2)
+    #4. timestamps(3) ,esp32 sends acknowledgement 
+    #5. raspberryy pi receives ack, timestamps(4)
+    #6. 
+
+# Handshake
+start_time = time.time() * 1000
+handshake = b'\x01'
+serial1.write(handshake)
+serial2.write(handshake)
+hs1 = False
+hs2 = False
+delay1 = 0
+delay2 = 0
+while hs1 == False | hs2 == False:
+    if serial1.readline().decode('utf-8').split(' ', 1)[0] == "Timestamp":
+        delay1 = (time.time() * 1000 - start_time) / 2
+        hs1 = True
+    if serial2.readline().decode('utf-8').split(' ', 1)[0] == "Timestamp":
+        delay2 = (time.time() * 1000 - start_time) / 2
+        hs2 = True
+
 # Run for 30 seconds
 while i < 30000:
     # Read serial data from ESP32 Things
@@ -32,49 +60,63 @@ while i < 30000:
     data1 = esp32Thing1.decode('utf-8').split(' ', 1).append(piTime)
     data2 = esp32Thing2.decode('utf-8').split(' ', 1).append(piTime)
     #   timestamp(esp32)[0], distance[1], timestamp(pi)[2]
+
     
-    # Save serial from each
-    timeline1.append(data1)
-    timeline2.append(data2)
+    # Save raw serial from each
+    raw_timeline1.append(data1)
+    raw_timeline2.append(data2)
+
+    # Apply offsets
+    data1[0] = data1[0] - offset1
+    data2[0] = data2[0] - offset2
+    
+    # Save synchronized serial from each
+    sync_timeline1.append(data1)
+    sync_timeline2.append(data2)
     
     # wait until there is a previous timestamp to reference
     if i > 0:
 
         # Even happens on sensor 1
-        if timeline1[i-1][1] > threshold & timeline1[i][1] < threshold:
+        if raw_timeline1[i-1][1] > threshold & raw_timeline1[i][1] < threshold:
             event1 = True
             eventIndex = eventIndex + 1
 
             if (event2):
-            # Both see event
-                events[eventIndex].append(data1)
+                # Both see event
+                events[eventIndex].append(raw_timeline1[i][0])
                 eventIndex = eventIndex + 1
+                event1 = False
+                event2 = False
+                offset1 = events[eventIndex][3] - events[eventIndex][0]
+                offset2 = 0
             else:
                 events.append(data1)
             
         # Even happens on sensor 2
-        if timeline2[i-1][1] > threshold & timeline2[i][1] < threshold:
+        if raw_timeline2[i-1][1] > threshold & raw_timeline2[i][1] < threshold:
             event2 = True
             eventIndex = eventIndex + 1
 
-            # Both see event
             if (event1):
-                events[eventIndex].append(data2)
-                
+                # Both see event
+                events[eventIndex].append(raw_timeline2[i][0])
                 eventIndex = eventIndex + 1
+                event1= False
+                event2= False
+                offset1 = 0
+                offset2 = events[eventIndex][3] - events[eventIndex][0]
             else:
                 events.append(data2)
                 
 
             
         # Drift calculation
-        drift1.append(timeline1[i][0] - timeline1[i-1][0] - 1)
-        drift2.append(timeline2[i][0] - timeline2[i-1][0] - 1)
+        drift1.append(raw_timeline1[i][0] - raw_timeline1[i-1][0] - 1)
+        drift2.append(raw_timeline2[i][0] - raw_timeline2[i-1][0] - 1)
         relativeDrift.append(drift1 - drift2)
 
-        # delay calcualation ?
-        
-
+       
     # serial output
     print(data1[0] + " " + data1[1])
     print(data2[0] + " " + data2[1])
